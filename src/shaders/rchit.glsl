39d02ec9fdb393ca6d4bad47d9415b32bdc769f5
Döring Christian <christian.doering@tum.de>
2022-06-23 23:55:09 +0200

    Inverted x axis of normal map as well.

--- src/shaders/rchit.glsl
+++ src/shaders/rchit.glsl
@@ -143,7 +143,7 @@ void main() {
         
         vec3 norm_tex = texture(textures[mat.normal_tex], texco).rgb;
         // need to invert the y component of the normal texture.
-        norm_tex = vec3(norm_tex.x, 1.-norm_tex.y, norm_tex.z);
+        norm_tex = vec3(1.-norm_tex.x, 1.-norm_tex.y, norm_tex.z);
         norm_tex = normalize(norm_tex * 2. - 1.);
         norm = normalize(TBN * norm_tex);
     }
