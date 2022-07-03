ef0abb8f727515d1094dc027bd1605b0abcb4081
Döring Christian <christian.doering@tum.de>
2022-07-03 14:08:47 +0200

    Pcg attribution

--- src/shaders/rand.glsl
+++ src/shaders/rand.glsl
@@ -13,18 +13,14 @@ float uint_to_unit_float(uint u){
 }
 
 /*
-	PCG random number generators ported to an NPM package, so that you can require it from glslify.
-	The code is based (mostly copied) from https://www.shadertoy.com/view/XlGcRh by Mark Jarzynski.
-	References:
-	* Mark Jarzynski and Marc Olano, Hash Functions for GPU Rendering, Journal of
-	  Computer Graphics Techniques (JCGT), vol. 9, no. 3, 21-38, 2020
-	  Available online http://jcgt.org/published/0009/03/02/
-	* https://www.pcg-random.org/
+Pcg Hashing algorithm copied from https://www.shadertoy.com/view/XlGcRh.
+ https://www.pcg-random.org/
 */
-uint pcg(uint v) {
-	uint state = v * uint(747796405) + uint(2891336453);
-	uint word = ((state >> ((state >> uint(28)) + uint(4))) ^ state) * uint(277803737);
-	return (word >> uint(22)) ^ word;
+uint pcg(uint v)
+{
+	uint state = v * 747796405u + 2891336453u;
+	uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
+	return (word >> 22u) ^ word;
 }
 
 float randf(inout uint seed){
