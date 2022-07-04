771867d35df5a5a96c34734ca769dda93a72c6bc
Döring Christian <christian.doering@tum.de>
2022-07-05 00:41:26 +0200

    OpConvertUToPtr needs to work

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder, SpirvMetadata};
 fn main() {
     SpirvBuilder::new("shaders/test01/", "spirv-unknown-vulkan1.2")
         .extension("SPV_KHR_ray_tracing")
+        .extension("SPV_KHR_physical_storage_buffer")
         .capability(Capability::RayTracingKHR)
         .capability(Capability::Int64)
         .capability(Capability::PhysicalStorageBufferAddresses)
--- shaders/test01/src/lib.rs
+++ shaders/test01/src/lib.rs
@@ -7,7 +7,7 @@
 #![deny(warnings)]
 #![allow(unused, dead_code)]
 
-use common::{Camera, Instance, Material, Payload};
+use common::{Camera, Instance, Material, Payload, Vertex};
 use rand::{rand2f, rand3f, randu};
 #[cfg(not(target_arch = "spirv"))]
 use spirv_std::macros::spirv;
@@ -121,18 +121,43 @@ pub fn main_rchit(
     #[spirv(descriptor_set = 0, binding = 1, storage_buffer)] instances: &[Instance],
     #[spirv(descriptor_set = 0, binding = 2, storage_buffer)] materials: &[Material],
     #[spirv(instance_custom_index)] index: u32,
+    #[spirv(primitive_id)] primitive_id: u32,
 ) {
     if ray.ray_active == 0 {
         return;
     }
 
-    //let inst = instances.get(index as usize).unwrap();
     let inst = instances[index as usize];
     let mat = materials[inst.mat_index as usize];
-    //let mut seed = inst.mat_index;
+    let transform = inst.transform;
 
-    //ray.color = rand3f(&mut seed);
-    ray.color = mat.albedo.xyz();
+    let indices: *mut RuntimeArray<u32> = unsafe { convert_u_to_ptr(inst.indices) };
+    let vertices: *mut RuntimeArray<Vertex> = unsafe { convert_u_to_ptr(inst.vertices) };
+
+    let i0 = unsafe { (*indices).index(3 * primitive_id as usize + 0) };
+    let i1 = unsafe { (*indices).index(3 * primitive_id as usize + 1) };
+    let i2 = unsafe { (*indices).index(3 * primitive_id as usize + 2) };
+
+    let vert0 = unsafe { (*vertices).index(*i0 as usize) };
+    let vert1 = unsafe { (*vertices).index(*i1 as usize) };
+    let vert2 = unsafe { (*vertices).index(*i2 as usize) };
+
+    let mut pos0 = vert0.pos.xyz();
+    let mut pos1 = vert1.pos.xyz();
+    let mut pos2 = vert2.pos.xyz();
+
+    pos0 = (transform * Vec4::from((pos0, 1.))).xyz();
+    pos1 = (transform * Vec4::from((pos1, 1.))).xyz();
+    pos2 = (transform * Vec4::from((pos2, 1.))).xyz();
+
+    let barycentric = vec3(1. - hit_co.x - hit_co.y, hit_co.x, hit_co.y);
+
+    let pos = pos0 * barycentric.x + pos1 * barycentric.y + pos2 * barycentric.z;
+
+    let gnorm = (pos1 - pos0).cross(pos2 - pos1).normalize();
+
+    //ray.color = mat.albedo.xyz();
+    ray.color = gnorm.xyz();
 
     //ray.color = vec3(1., 0., 0.);
     ray.ray_active = 0;
