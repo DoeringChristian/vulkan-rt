5a7eff192a4bd2d23911d9b7b92768de91dd19d6
Döring Christian <christian.doering@tum.de>
2022-07-05 10:37:06 +0200

    Fixed glam-macros bug

--- shaders/test01/src/glam_macro.rs
+++ shaders/test01/src/glam_macro.rs
@@ -1,43 +1,49 @@
+macro_rules! vec2 {
+    ($($v:expr),*) => {
+        Vec2::from(($($v),*))
+    }
+}
+macro_rules! vec3 {
     ($($v:expr),*) => {
         Vec3::from(($($v),*))
-    };
+    }
 }
 macro_rules! vec4 {
     ($($v:expr),*) => {
         Vec4::from(($($v),*))
-    };
+    }
 }
 
 macro_rules! ivec2 {
     ($($v:expr),*) => {
         IVec2::from(($($v),*))
-    };
+    }
 }
 macro_rules! ivec3 {
     ($($v:expr),*) => {
         IVec3::from(($($v),*))
-    };
+    }
 }
 macro_rules! ivec4 {
     ($($v:expr),*) => {
         IVec4::from(($($v),*))
-    };
+    }
 }
 
 macro_rules! uvec2 {
     ($($v:expr),*) => {
         UVec2::from(($($v),*))
-    };
+    }
 }
 macro_rules! uvec3 {
     ($($v:expr),*) => {
         UVec3::from(($($v),*))
-    };
+    }
 }
 macro_rules! uvec4 {
     ($($v:expr),*) => {
         UVec4::from(($($v),*))
-    };
+    }
 }
 
 macro_rules! mat2{
