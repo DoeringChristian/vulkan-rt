b437793e8f6538629c5205f97e52c7bf11b0b494
Döring Christian <christian.doering@tum.de>
2022-07-11 10:45:19 +0200

    added extensions needed for device references

--- src/shaders/rchit.glsl
+++ src/shaders/rchit.glsl
@@ -2,6 +2,9 @@
 #version 460
 #extension GL_EXT_ray_tracing : require
 #extension GL_EXT_nonuniform_qualifier : enable
+#extension GL_EXT_buffer_reference2 : require
+#extension GL_EXT_scalar_block_layout: require
+#extension GL_EXT_shader_explicit_arithmetic_types_int64 : require
 
 #include "rand.glsl"
 #include "common.glsl"
--- src/shaders/rgen.glsl
+++ src/shaders/rgen.glsl
@@ -1,5 +1,6 @@
 #version 460
 #extension GL_EXT_ray_tracing : require
+#extension GL_EXT_shader_explicit_arithmetic_types_int64 : require
 
 #include "rand.glsl"
 #include "common.glsl"
